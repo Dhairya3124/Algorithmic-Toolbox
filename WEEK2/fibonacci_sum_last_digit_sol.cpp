eb061fbd5f674cd20957fc07c614acce774ee4eb
Dhairya Khanna <dk9582671597@gmail.com>
2020-06-02 20:28:26 +0530

    Q6

---
+++ WEEK2/fibonacci_sum_last_digit_sol.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define ll  long long int 
+ll period(ll m) {
+	ll a = 0, b = 1, c = a + b; 
+	for (int i = 0; i < m*m; i++) {
+		c = (a + b) % m; 
+		a = b; 
+		b = c; 
+		if (a == 0 && b == 1)
+			return i + 1; 
+	}
+}
+ll get_fibonacci_huge_naive(ll n, ll m) {
+	ll rem = n%period(m);
+	ll x = 0;
+	ll y = 1;
+	ll ans = rem;
+	for(ll i=1;i<rem;i++)
+	{
+		ans = (x+y)%m;
+		x=y;
+		y=ans;
+	}
+	return ans%m;
+}
+ll solve(ll n)
+{
+	ll n2 = get_fibonacci_huge_naive(n+2,10);
+	ll n1 = get_fibonacci_huge_naive(2,10);
+	if(n2>=n1)
+		return n2-n1;
+	else
+		return (10+n2)-n1;
+}
+int main() {
+    ll n;
+    std::cin >> n ;
+    std::cout << solve(n) << '\n';
+}
