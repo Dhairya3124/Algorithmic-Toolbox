d3021b6a506de0a9ea73b41deb5eebb451eb0202
Dhairya Khanna <dk9582671597@gmail.com>
2020-06-02 19:48:17 +0530

    Q5

---
+++ WEEK2/fibonacci_hugesol.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define ll  long long int 
+ll period(ll m) {
+	ll a = 0, b = 1, c = a + b; 
+	for (int i = 0; i < m*m; i++) {
+		c = (a + b) % m; 
+		a = b; 
+		b = c; 
+		if (a == 0 && b == 1)
+			return i + 1; 
+	}
+}
+ll get_fibonacci_huge_naive(ll n, ll m) {
+	ll rem = n%period(m);
+	ll x = 0;
+	ll y = 1;
+	ll ans = rem;
+	for(ll i=1;i<rem;i++)
+	{
+		ans = (x+y)%m;
+		x=y;
+		y=ans;
+	}
+	return ans%m;
+}
+
+int main() {
+    ll n, m;
+    std::cin >> n >> m;
+    std::cout << get_fibonacci_huge_naive(n, m) << '\n';
+}
